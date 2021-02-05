b6df98760d2aadc61b7f868d0265dd6a9b4079ce
pcdogyu <aaa@sap.com>
2021-02-05 13:24:07 +0800

    add all 4 script to testing disk io

---
+++ 01read.sh
@@ -0,0 +1,1 @@
+for i in 4k 8k 16k 32k 128k; do echo $i >> /raid5/read.txt && fio -filename=/raid5/001  -direct=1 -iodepth 32 -thread -rw=read -ioengine=libaio -bs=$i -size=30g -numjobs=30 -runtime=60 -group_reporting -name=sap>>/raid5/read.txt && rm -rf /raid5/001; done
---
+++ 02write.sh
@@ -0,0 +1,1 @@
+for i in 4k 8k 16k 32k 128k; do echo $i >> /raid5/write.txt && fio -filename=/raid5/002  -direct=1 -iodepth 32 -thread -rw=write -ioengine=libaio -bs=$i -size=30g -numjobs=30 -runtime=60 -group_reporting -name=sap>>/raid5/write.txt && rm -rf /raid5/002; done
