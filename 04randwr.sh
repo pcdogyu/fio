fe9fc797667ff14e5dc7020ebe24cf8067ccc2c3
pcdogyu <aaa@sap.com>
2021-02-05 18:38:17 +0800

    update time

--- 04randwr.sh
+++ 04randwr.sh
@@ -1,1 +1,1 @@
-for i in 4k 8k 16k 32k 128k; do echo $i >> /raid5/randrw.txt && fio -filename=/raid5/004  -direct=1 -iodepth 32 -thread -rw=randrw -ioengine=libaio -bs=$i -size=30g -numjobs=30 -runtime=60 -group_reporting -name=sap>>/raid5/randrw.txt && rm -rf /raid5/004; done
+for i in 4k 8k 16k 32k 128k; do echo $i >> /raid5/randrw.txt && fio -filename=/raid5/004  -direct=1 -iodepth 32 -thread -rw=randrw -ioengine=libaio -bs=$i -size=30g -numjobs=30 -runtime=180 -group_reporting -name=sap>>/raid5/randrw.txt && rm -rf /raid5/004; done
